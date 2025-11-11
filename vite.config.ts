f3aa2361f07edc438aafc552ddaef22dfc370471
Whitigol <whitetigerwolfthe1st@gmail.com>
2025-11-11 17:30:25 -0500

    fix: disable prerender temporarily

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
-import react from '@vitejs/plugin-react';
-import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import { defineConfig } from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from '@tailwindcss/vite';
-import mdx from 'fumadocs-mdx/vite';
+import react from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "fumadocs-mdx/vite";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [
-    mdx(await import('./source.config')),
-    tailwindcss(),
-    tsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tanstackStart({
-      prerender: {
-        enabled: true,
-      },
-    }),
-    react(),
-  ],
+	server: {
+		port: 3000,
+	},
+	plugins: [
+		mdx(await import("./source.config")),
+		tailwindcss(),
+		tsConfigPaths({
+			projects: ["./tsconfig.json"],
+		}),
+		tanstackStart({
+			prerender: {
+				enabled: false,
+			},
+			sitemap: {
+				enabled: true,
+				host: "v2.docs.whitigol.me",
+			},
+		}),
+		react(),
+	],
 });
